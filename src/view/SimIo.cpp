aaa85436ab2de788b9248836d9d9b2caaf25f899
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-03 10:23:18 -0600

    Created SimIO class

---
+++ src/view/SimIo.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ethanscharlie on 6/3/25.
+//
+
+#include "SimIo.hpp"
+
+namespace View {
+} // View
---
+++ src/view/SimIo.hpp
@@ -0,0 +1,17 @@
+//
+// Created by ethanscharlie on 6/3/25.
+//
+
+#ifndef SIMIO_HPP
+#define SIMIO_HPP
+#include "DeviceIo.hpp"
+
+namespace View {
+
+class SimIO : public DeviceIO {
+
+};
+
+} // View
+
+#endif //SIMIO_HPP
