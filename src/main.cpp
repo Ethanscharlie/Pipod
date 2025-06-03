b1f76af34f281e8d351d84817b7d6e41dcf36b78
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-03 14:06:23 -0600

    Working sdl2 window

--- src/main.cpp
+++ src/main.cpp
@@ -16,5 +16,6 @@ int main(int argc, char **argv) {
 
   while (true) {
     signal(SIGINT, [](int s) { std::exit(1); });
+    deviceIO->refreshDisplay();
   }
 }
--- src/view/SimIo.cpp
+++ src/view/SimIo.cpp
@@ -3,12 +3,15 @@
 //
 
 #include "SimIo.hpp"
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
 
 namespace View {
 SimIO::SimIO() {
   SDL_Init(SDL_INIT_VIDEO);
 
-  window = SDL_CreateWindow("Pipod Simulator", 0, 0, DISPLAY_WIDTH,
+  window = SDL_CreateWindow("Pipod Simulator", SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, DISPLAY_WIDTH,
                             DISPLAY_HEIGHT, SDL_WINDOW_SHOWN);
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
@@ -17,5 +20,8 @@ bool SimIO::isButtonDown() { return false; }
 
 void SimIO::refreshDisplay() { SDL_RenderPresent(renderer); }
 
-void SimIO::setDisplayColor(Color color) {}
+void SimIO::setDisplayColor(Color color) {
+  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+  SDL_RenderClear(renderer);
+}
 } // namespace View
