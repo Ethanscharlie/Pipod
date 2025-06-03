73092dff972243f06b8f5057fbddd2096d2a2ae1
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-03 10:12:05 -0600

    Moved tests into tests file

--- tests/model/MediaManager.cpp
+++ tests/model/MediaManager.cpp
@@ -6,10 +6,29 @@
 
 #include <gtest/gtest.h>
 
-// TEST(MediaManagerTest, PlayAudioFile) {
-//     Model::MediaManager mediaManager;
-//     mediaManager.loadFile("../tests/res/audiotest.mp3");
-//     mediaManager.play();
-//
-//     EXPECT_FALSE(mediaManager.isPlaying());
-// }
+TEST(MediaManagerTest, testPlayWithoutLoadedFile) {
+    Model::MediaManager mediaManager;
+    mediaManager.play();
+}
+
+TEST(MediaManagerTest, testStopWithoutLoadedFile) {
+    Model::MediaManager mediaManager;
+    mediaManager.stop();
+}
+
+TEST(MediaManagerTest, PlayAudioFile) {
+    std::filesystem::path file = "../tests/res/audiotest.mp3";
+    if (!std::filesystem::exists(file)) {
+        FAIL() << "File does not exist";
+    }
+
+    Model::MediaManager mediaManager;
+    mediaManager.loadFile(file);
+    mediaManager.play();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    EXPECT_TRUE(mediaManager.isPlaying());
+    mediaManager.stop();
+}
+
