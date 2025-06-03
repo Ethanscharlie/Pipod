092af5889363562e416211f77d747eb70ea087bf
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-03 14:01:21 -0600

    Testing and endless loop working

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
+#include <csignal>
 #include <gtest/gtest.h>
+#include <iostream>
 
 #include "model/MediaManager.hpp"
 #include "view/DeviceIo.hpp"
 #include "view/SimIo.hpp"
 
 int main(int argc, char **argv) {
-    // testing::InitGoogleTest(&argc, argv);
-    // return RUN_ALL_TESTS();
+  // testing::InitGoogleTest(&argc, argv);
+  // return RUN_ALL_TESTS();
 
-    View::SimIO simIO = View::SimIO();
-    View::DeviceIO* deviceIO = &simIO;
-    deviceIO->isButtonDown();
+  View::SimIO simIO = View::SimIO();
+  View::DeviceIO *deviceIO = &simIO;
+  deviceIO->isButtonDown();
 
-    while (true) {
-        signal(SIGINT, SIG_IGN);
-    }
+  while (true) {
+    signal(SIGINT, [](int s) { std::exit(1); });
+  }
 }
--- src/view/SimIo.cpp
+++ src/view/SimIo.cpp
@@ -5,21 +5,17 @@
 #include "SimIo.hpp"
 
 namespace View {
-    SimIO::SimIO() {
-        SDL_Init(SDL_INIT_VIDEO);
+SimIO::SimIO() {
+  SDL_Init(SDL_INIT_VIDEO);
 
-        window = SDL_CreateWindow("Pipod Simulator", 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, SDL_WINDOW_SHOWN);
-        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    }
+  window = SDL_CreateWindow("Pipod Simulator", 0, 0, DISPLAY_WIDTH,
+                            DISPLAY_HEIGHT, SDL_WINDOW_SHOWN);
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
 
-    bool SimIO::isButtonDown() {
-        return false;
-    }
+bool SimIO::isButtonDown() { return false; }
 
-    void SimIO::refreshDisplay() {
-        SDL_RenderPresent(renderer);
-    }
+void SimIO::refreshDisplay() { SDL_RenderPresent(renderer); }
 
-    void SimIO::setDisplayColor(Color color) {
-    }
-} // View
+void SimIO::setDisplayColor(Color color) {}
+} // namespace View
