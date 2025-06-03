d693c07c2d58ade6fa73d4b487a3658e72e31109
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-03 06:51:57 -0600

    Created MediaManager class

---
+++ model/MediaManager.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ethanscharlie on 6/3/25.
+//
+
+#include "MediaManager.hpp"
+
+namespace Model {
+} // Model
---
+++ model/MediaManager.hpp
@@ -0,0 +1,16 @@
+//
+// Created by ethanscharlie on 6/3/25.
+//
+
+#ifndef MEDIAMANAGER_HPP
+#define MEDIAMANAGER_HPP
+
+namespace Model {
+
+class MediaManager {
+
+};
+
+} // Model
+
+#endif //MEDIAMANAGER_HPP
