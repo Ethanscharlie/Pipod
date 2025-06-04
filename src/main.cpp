cfc9bf00a236aa803be321c1ae8184f700fa425a
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-03 20:52:44 -0600

    All inputs working in sim

--- src/main.cpp
+++ src/main.cpp
@@ -23,14 +23,26 @@ void drawInputTest(View::DeviceIO *deviceIO) {
   const std::pair<int, int> size = {20, 20};
   View::Color color = {255, 255, 255};
 
+  color = getColorOfInputTest(deviceIO->isJoystickUpDown());
+  deviceIO->drawRect({30, 30}, size, color);
+
+  color = getColorOfInputTest(deviceIO->isJoystickDownDown());
+  deviceIO->drawRect({30, 80}, size, color);
+
+  color = getColorOfInputTest(deviceIO->isJoystickRightDown());
+  deviceIO->drawRect({50, 54}, size, color);
+
+  color = getColorOfInputTest(deviceIO->isJoystickLeftDown());
+  deviceIO->drawRect({10, 54}, size, color);
+
   color = getColorOfInputTest(deviceIO->isButton1Down());
-  deviceIO->drawRect({10, 10}, size, color);
+  deviceIO->drawRect({40, 110}, size, color);
 
   color = getColorOfInputTest(deviceIO->isButton2Down());
-  deviceIO->drawRect({40, 10}, size, color);
+  deviceIO->drawRect({70, 110}, size, color);
 
   color = getColorOfInputTest(deviceIO->isButton3Down());
-  deviceIO->drawRect({70, 10}, size, color);
+  deviceIO->drawRect({100, 110}, size, color);
 }
 
 int main(int argc, char **argv) {
